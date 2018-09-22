44b30c693e3e04b6dc7b3c9b00e138cc469470d2
jackie <jacquelyn.cheng@me.com>
2018-09-21 21:51:08 -0700

    Implemented restful routes and views

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,22 +1,55 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
   end
 
   def show
+    @animal = Animal.find_by(id: params[:id].to_i)
+
+    if @animal.nil?
+      render :notfound, status: :not_found
+    end
   end
 
   def new
+    @animal = Animal.new
   end
 
   def create
+    animal = Animal.new(animal_params)
+    if animal.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @animal = Animal.find_by(id: params[:id].to_i)
   end
 
   def update
+    animal = Animal.find_by(id: params[:id].to_i)
+    animal.update(animal_params)
+    if animal.save
+      redirect_to animal_path(animal.id)
+    else
+      render :new
+    end
   end
 
   def destroy
+    animal = Animal.find_by(id: params[:id].to_i)
+    if Animal.delete(animal)
+      redirect_to root_path
+    else
+      render :not_found
+    end
   end
+
+  private
+
+    def animal_params
+      return params.require(:animal).permit(:name, :species, :age, :image)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'animals/index'
-  get 'animals/show'
-  get 'animals/new'
-  get 'animals/create'
-  get 'animals/edit'
-  get 'animals/update'
-  get 'animals/destroy'
+  root 'animals#index'
+  resources :animals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
